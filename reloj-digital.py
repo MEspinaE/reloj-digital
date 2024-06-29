e7e6961791f71974044e3bc76a94fa360d483039
MEspinaE <168482425+MEspinaE@users.noreply.github.com>
2024-06-28 22:30:56 -0400

    primer commit

---
+++ reloj-digital.py
@@ -0,0 +1,17 @@
+import tkinter as tk
+import time
+
+def actualiza_reloj():
+    current_time = time.strftime("%H:%M:%S")
+    clock_label.config(text=current_time)
+    root.after(1000, actualiza_reloj)  
+
+root = tk.Tk()
+root.title("Reloj Digital")
+
+clock_label = tk.Label(root, font=("Times new roman", 35))
+clock_label.pack()
+
+actualiza_reloj() 
+
+root.mainloop()
